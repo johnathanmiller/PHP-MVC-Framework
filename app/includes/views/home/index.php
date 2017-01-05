dfaa62fe9f79d488f456b85b5c2de84b49c4b026
Johnathan Miller <johnathan@pebblebee.com>
2017-01-04 20:11:35 -0800

    Updated homepage layout with sidebar, imported FontAwesome stylesheet in header, and added rules to stylesheet

--- app/includes/views/home/index.php
+++ app/includes/views/home/index.php
@@ -1,13 +1,16 @@
 
 			<div class="section-wrapper main">
 				<div class="container">
-					<div class="col-sm-12 text-center">
+					<div class="col-sm-9 text-left">
 						<div class="content-header">
 							<h2>Index</h2>
 						</div>
 						<div class="content-body">
 							<p>This is the home page</p>
 						</div>
+					</div>
+					<div class="col-sm-3">
+						<?php include_once 'template/sidebar.php'; ?>
 					</div>
 				</div>
 			</div>
