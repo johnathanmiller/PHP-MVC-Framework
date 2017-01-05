161a362bf8676cf132eb42d50dd6c8f8dd893536
Johnathan Miller <johnathan@pebblebee.com>
2017-01-04 20:20:26 -0800

    Added sidebar.php file meant to be included in last commit

---
+++ app/includes/views/home/template/sidebar.php
@@ -0,0 +1,8 @@
+	<aside>
+		<a href="https://github.com/johnathanmiller/PHP-MVC-Framework" class="btn btn-primary" title="<?php print SITE_NAME; ?> on GitHub">
+			<i class="fa fa-github fa-fw"></i> View on GitHub
+		</a>
+		<a href="https://johnathanmiller.com" class="btn btn-default">
+			<i class="fa fa-globe fa-fw"></i> johnathanmiller.com
+		</a>
+	</aside>
