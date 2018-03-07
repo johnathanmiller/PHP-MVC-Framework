6b10feca12b6ac8410b4b5dd9ed3b534e572484a
Johnathan Miller <johnathanmiller@users.noreply.github.com>
2018-03-06 23:45:45 -0800

    Updated autoloader

--- app/autoload.php
+++ app/autoload.php
@@ -7,30 +7,20 @@ require_once (in_array($_SERVER['SERVER_ADDR'], ['::1', '127.0.0.1'])) ? './app/
 
 function autoloader($class) {
 
-	$controllers_path	= CONTROLLERS . $class .'.php';
-	$lib_path			= LIB . $class .'.php';
-	$models_path		= MODELS . $class .'.php';
-	$storage_path		= STORAGE . $class .'.php';
+	$directories = [
+		CONTROLLERS,
+		LIB,
+		MODELS,
+		STORAGE
+	];
 
-	if (file_exists($controllers_path)) {
-
-		require_once $controllers_path;
-
-	} else if (file_exists($lib_path)) {
-
-		require_once $lib_path;
-
-	} else if (file_exists($models_path)) {
-
-		require_once $models_path;
-
-	} else if (file_exists($storage_path)) {
-
-		require_once $storage_path;
-
-	} else {
-
-		throw new Exception('Failed to include class: '. $class);
-
+	foreach ($directories as $directory) {
+		if (file_exists($directory . $class . '.php')) {
+			require_once $directory . $class . '.php';
+			return;
+		}
 	}
+
+	throw new Exception('Failed to include class: '. $class);
+	
 }
